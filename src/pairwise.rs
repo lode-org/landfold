a8bc96ffa464c3e8584473ea04c2eb6fd979a352
Rohit Goswami <rgoswami@ieee.org>
2026-08-19 17:52:45 -0500

    test: cover strided pairwise inputs

--- src/pairwise.rs
+++ src/pairwise.rs
@@ -274,6 +274,14 @@ mod tests {
     }
 
     #[test]
+    fn accepts_non_contiguous_point_rows() {
+        let points = array![[0.0, 0.0, 9.0], [3.0, 4.0, 9.0]];
+        let strided = points.view().reversed_axes();
+        let distances = pairwise_euclid(strided).unwrap();
+        assert_relative_eq!(distances[(0, 1)], 5.0, epsilon = 1e-14);
+    }
+
+    #[test]
     fn transfer_application_is_checked_and_transactional() {
         let transfer = crate::transfer::Transfer::identity();
         let mut nonsquare = Array2::<f64>::zeros((2, 1));
